8168ece9e1bc3f2d23731a42b26f94f2e7eedfca
Martin Jul <no-spam@mjul.com>
2019-09-13 22:13:14 +0200

    Add enum example with simple when patterns

---
+++ src/patternmatching/withenums/withenums.kt
@@ -0,0 +1,29 @@
+package patternmatching.withenums
+
+// Pattern matching in Kotlin is done with the when function
+enum class Shape {
+    POINT,
+    CIRCLE,
+    TRIANGLE,
+    RECTANGLE
+}
+
+fun stringifyShape(shape:Shape) =
+    when(shape) {
+        // when matches the first match only (unlike switch it does not fall through if break; is missing)
+        // when on enums must be exhaustive or have a default
+        // if you remove one of the matches below it will not compile
+        // also, if you add another Shape to the enum it will not compile until you add a matching case
+        Shape.POINT -> "point"
+        Shape.CIRCLE -> "circle"
+        Shape.TRIANGLE -> "triangle"
+        Shape.RECTANGLE -> "rectangle"
+    }
+
+fun stringifyShapeWithDefault(shape: Shape) =
+    when(shape) {
+        // if you add an else case it does not have to be exhaustive
+        Shape.CIRCLE -> "circle"
+        else -> "unknown"
+    }
+
